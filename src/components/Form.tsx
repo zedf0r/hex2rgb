bafea3fc07640765c2b1859e0d435e85e11291dc
Leonid <zemlyanykh@bk.ru>
2025-04-10 11:23:15 +0500

    Update form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,15 +6,18 @@ export const Form = () => {
     const { target } = evt;
     const { value } = target as HTMLInputElement;
 
-    if (value.length === 6) {
-      const r = parseInt(value.slice(0, 2), 16);
-      const g = parseInt(value.slice(2, 4), 16);
-      const b = parseInt(value.slice(4, 6), 16);
+    if (value.length === 7) {
+      const hex = value.slice(0, 1);
+      if (hex === "#") {
+        const r = parseInt(value.slice(1, 3), 16);
+        const g = parseInt(value.slice(3, 5), 16);
+        const b = parseInt(value.slice(5, 7), 16);
 
-      setColor(`rgb(${r}, ${g}, ${b})`);
+        setColor(`rgb(${r}, ${g}, ${b})`);
 
-      if (Number.isNaN(r) || Number.isNaN(g) || Number.isNaN(b)) {
-        setColor("Ошибка");
+        if (Number.isNaN(r) || Number.isNaN(g) || Number.isNaN(b)) {
+          setColor("Ошибка");
+        }
       }
     }
   };
